a9896805d30d3eda7544a8fa944cdb2dcc52d301
Billy Shih <me@billyshih.com>
2013-06-19 14:24:45 -0700

    setup carrier wave and photo scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoBlog::Application.config.secret_token = '781c2de4ddfb73b6a1a5a2a8151b2d8d4c5c49bca027e4b79f87d9b7dacdea5aa199ba11bc42992cd9026fddead9448e257d3e898ebd7e7484ab8162cd731a3d'
