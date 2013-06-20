496365ee643b605e94b9eeeb241af1b1fc8b603b
Billy Shih <me@billyshih.com>
2013-06-20 09:10:41 -0700

    Changed parameters

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
-    aws_access_key_id: AKIAIUOWTBKWZU7PIYPQ,
-    aws_secret_access_key: 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
+    aws_access_key_id: "AKIAIUOWTBKWZU7PIYPQ",
+    aws_secret_access_key: "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"
   }
-  config.fog_directory = corgi-photo-blog
+  config.fog_directory = "corgi-photo-blog"
 end
